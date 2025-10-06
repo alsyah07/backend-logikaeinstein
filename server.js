1e411445c4d555f8f175afd1181cfaed5ab8fc0e
alsyah07 <46066495+alsyah07@users.noreply.github.com>
2025-10-06 08:41:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3100;
 
 // Middleware
 app.use(cors());
